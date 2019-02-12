c80737aaf521e9ec1daa524573f3afd427ee3d48
Vinicius <vnsqzs@gmail.com>
2019-02-11 21:28:55 -0300

    finish build login page with MDC (Material Design Componentes by Google)

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
-import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
+  new MDCRipple(document.querySelector('.cancel'));
+  new MDCRipple(document.querySelector('.next'));
 
-const username = new MDCTextField(document.querySelector('.username'));
-const password = new MDCTextField(document.querySelector('.password'));
+import {MDCTextField} from '@material/textfield';
+  const username = new MDCTextField(document.querySelector('.username'));
+  const password = new MDCTextField(document.querySelector('.password'));
